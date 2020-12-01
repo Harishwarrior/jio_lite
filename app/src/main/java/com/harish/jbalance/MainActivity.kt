6c796f8b8678f797e09a4929bc389590cbc7da23
harishwarrior <warriorharish95668@gmail.com>
2020-12-01 11:19:06 +0530

    Initial commit

---
+++ app/src/main/java/com/harish/jbalance/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.harish.jbalance
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        webView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                if (url != null) {
+                    view?.loadUrl(url)
+                }
+                return true
+            }
+        }
+        webView.loadUrl("https://www.jio.com/dashboard")
+    }
+}
