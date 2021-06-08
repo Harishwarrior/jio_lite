4bd5ce1edd6ff7d22bae192b53803f7ef89005b7
harishwarrior <warriorharish95668@gmail.com>
2021-06-08 17:19:18 +0530

    fixed error

--- app/src/main/java/com/harish/jio_lite/MainActivity.kt
+++ app/src/main/java/com/harish/jio_lite/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-a
+
         webView = findViewById(R.id.webView)
 
         webView.settings.javaScriptEnabled = true
